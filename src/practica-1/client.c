73d76beefeef2195b0edae9e6cd0ed04c29c55cc
lburgos005 <34044523+lburgos005@users.noreply.github.com>
2018-04-28 12:54:02 +0200

    Update client.c

--- src/practica-1/client.c
+++ src/practica-1/client.c
@@ -1,57 +1,237 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Laura
+ *
+ * Created on 6 de febrero de 2018, 11:01
+ */
 
 #include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pthread.h>
+void handler();
+void lb();
+void ejercicio1();
+void ejercicio2();
+void ejercicio3();
+void *actividad();
+void crearHilo();
+void cambiarFin();
+void ejercicio4();
+void startCount();
+void contarSen();
+void stopSet();
+void ejercicio5();
+void ejercicio6();
 
-#include "client.h"		// Include propio en directorio actual
+pid_t hijo1, hijo2;
+sig_atomic_t fin = 0, stop = 0;
+int pidMoni, numSen= 0;
 
-#define MIN_PARAMS 2		// EL nombre de programa es un parametro
-#define MAX_PARAMS 2		// Incluido el nombre de programa
-
-/********************* PROGRAMA DE LA PRACTICA X **************************/
-/* Historia:
-
-28/02/2017	AFV	Inicio codificación
-
-*/
-
-/*------------------------- Mensaje de información -----------------------*/
-
-void print_usage(char *command)
-{
-	printf("El formato del comando es:\n");
-	printf("%s <id proceso MONITOR>\n",command);
+void handler(){
+    kill(pidMoni, SIGUSR2);
 }
 
-/*------------------------------------------------------------------------*/
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    pidMoni= atoi(argv[1]);
+    char ejercicio;
+    while(1){
+        
+        printf("Pulsa el numero de ejercicio (1-6)\n");
+        lb();
+        scanf("%c", &ejercicio);
+        switch(ejercicio){
+            case '1':{
+                  ejercicio1();
+            }break;
 
-/*--------------------- Imprimir parametros ------------------------------*/
+            case '2':{
+                ejercicio2();
 
-void print_parameters(int argc, char *argv[])
-{
-int i;
-	for(i=0;i<argc;i++)
-		printf("PAR %d: <%s>\n",i,argv[i]);
+            }break;
+
+            case '3':{
+                ejercicio3();
+
+            }break;
+
+            case '4':{
+
+                ejercicio4();
+            }break;
+
+	    case '5':{
+
+                ejercicio5();
+            }break;
+		
+	    case '6':{
+
+                ejercicio6();
+            }break;
+           
+            case '0':
+            {
+                //SALIR
+                
+            }break;
+
+        }
+
+    }
+
+    
+    
+
+    return (EXIT_SUCCESS);
 }
 
-/*------------------------------------------------------------------------*/
-
-/*---------------------- Función principal -------------------------------*/
-/* Comentario:
-Conviene que esta función sea fácil de leer y de una idea de la forma de funcionar la lógica del programa. Apóyate en funciones auxiliares para conseguir este objetivo.
-*/
-
-int main(int argc, char *argv[])
-{
-	if(argc < MIN_PARAMS || argc > MAX_PARAMS) {
-		fprintf(stderr,"Mal formato de comando.\n");
-		print_usage(argv[0]);
-		exit(1);
-	}
-	print_parameters(argc,argv);
-	exit(0);
+void lb(){
+    fflush(stdout);
+    __fpurge(stdin);
 }
 
-/*------------------------------------------------------------------------*/
+void ejercicio1(){
+    printf("Ejercicio1...\n");
+    kill(pidMoni, SIGUSR1);
+    sleep(5);
+}
 
-/**************************************************************************/
+void ejercicio2(){
+    printf("Ejercicio2...\n");
+    kill(pidMoni, SIGUSR1);
+    signal(SIGUSR2, handler);
+            
+}
+
+void ejercicio3(){
+    int status1, status2;
+    printf("Ejercicio3...\n");
+    kill(pidMoni, SIGUSR1);
+    sleep(1);
+    
+    if((hijo1= fork()) == 0){
+        //este es el hijo1
+    }
+    else if((hijo2= fork()) == 0){
+        //Este es el hijo2
+    }
+    //este es el padre
+    kill(pidMoni , SIGUSR1);
+    waitpid(hijo1, &status1, 0);
+    waitpid(hijo2, &status2, 0);
+
+                  
+}
+
+void *actividad(){
+    int i;
+    for(i= 0; i< 4; i++){
+        printf("Soy el thread %d\n", (int) pthread_self());
+        sleep(5);
+    }
+    pthread_exit(0);
+}
+
+void crearHilo(){
+    int numHilos = 0; 
+    pthread_t hilo;
+    pthread_create(&hilo, NULL, actividad, (void*)NULL);
+    numHilos++;
+    printf("Hilo creado, este es el hijo %d\n", numHilos);
+}
+
+void cambiarFin(){
+    fin = 1;
+    
+}
+
+void ejercicio4(){
+	printf("Ejercicio 4...");
+    int status1, status2;
+    if((hijo1= fork()) == 0){
+        //este es el hijo1
+        signal(SIGUSR2, crearHilo);
+	
+        signal(SIGALRM, cambiarFin);
+        alarm(20);
+        while(!fin);
+    }
+    else if((hijo2= fork()) == 0){
+        //Este es el hijo2
+    }
+    //este es el padre
+    waitpid(hijo1, &status1, 0);
+    waitpid(hijo2, &status2, 0);
+
+}
+
+void startCount(){
+
+	alarm(60);
+	signal(SIGUSR1, stopSet);
+
+
+}
+
+void contarSen(){
+	numSen++;
+	kill(pidMoni, SIGUSR2);
+	printf("Señal %d", numSen);
+}
+
+void stopSet(){
+
+	stop= 1;
+}
+
+void ejercicio5(){
+	printf("Ejercicio 5...");
+    int status1, status2;
+    kill(pidMoni, SIGUSR1);
+
+    if((hijo1= fork()) == 0){
+        //este es el hijo1
+      
+    }
+    else if((hijo2= fork()) == 0){
+        //Este es el hijo2
+	signal(SIGUSR1, startCount);
+	signal(SIGALRM, stopSet); 
+	signal(SIGUSR2, contarSen);
+	while(stop == 0);
+    }
+    //este es el padre
+    waitpid(hijo1, &status1, 0);
+    waitpid(hijo2, &status2, 0);
+
+}
+
+void ejercicio6(){
+	
+	printf("Ejercicio 6...");
+ 
+    kill(pidMoni, SIGUSR1);
+    sleep(1);
+    kill(hijo1, SIGKILL);
+    kill(hijo2, SIGKILL);
+
+
+
+}
+
+
 
